de99e9f175448fb6239d07171fd3727f535fa642
Chaiyaphum Siripanpornchana <big.qmlh@gmail.com>
2025-11-24 14:16:03 +0700

    Add files via upload

---
+++ login/resources/js/login.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.getElementsByClassName('kc-logo-text')[0].addEventListener('click', function () {
+        location.href = 'https://scoring.sme.go.th'
+    }, false);
+
+    document.getElementsByTagName("a")[1].href = "https://scoring.sme.go.th/register";
+}
